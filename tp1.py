77c8599990571ca6c04692d004ec86f6ebad3d06
Martin Zahnd <mzahnd@itba.edu.ar>
2021-04-03 16:00:01 -0300

    Added License and file information

--- tp1.py
+++ tp1.py
@@ -1,6 +1,20 @@
-"""
+"""Trabajo práctico EAC1
 
-Fuentes:
+93.24 - Probabilidad y Estadística
+
+Docentes:
+.- Villaverde, Francisco
+.- Pantazis, Lucio
+.- Cosatto, Pedro
+
+Alumnos:
+.- Carolo, Lorena
+.- Pardiñas, Victoria
+.- Peydro, Florencia
+.- Scalise, Zarina
+.- Zahnd, Martín E.
+
+Fuentes y documentación consultadas:
     Documentación de matplotlib
     https://matplotlib.org/stable/tutorials/index.html
     https://matplotlib.org/stable/gallery/lines_bars_and_markers/marker_reference.html
@@ -17,6 +31,30 @@ Fuentes:
 
     RealPython
     https://realpython.com/numpy-scipy-pandas-correlation-python/
+
+
+Licencia:
+MIT License
+
+Copyright (c) 2021 Martín E. Zahnd
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
 import copy
 import math
@@ -732,7 +770,7 @@ def ejercicio_J(df, columna):
     var_menor_p = df[columna].tolist()
 
     # TODO: Conseguir datos reales
-    mediciones_alumnos = [84, 423, 653, 954, 135]
+    mediciones_alumnos = [84, 85, 653, 954, 135]
 
     # Las funciones polyval y polyfit de Numpy son el reemplazo de las
     # funciones homónimas en Octave/Matlab.
